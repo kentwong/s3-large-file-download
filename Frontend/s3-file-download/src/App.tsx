81b453361d86a80baa7d4e95e0dffb1df38091bf
Kent Wong <kent_wong@outlook.com>
2023-11-17 00:17:42 +1100

    update to axois

--- Frontend/s3-file-download/src/App.tsx
+++ Frontend/s3-file-download/src/App.tsx
@@ -1,55 +1,29 @@
 import React, { useState } from "react";
+import axios from "axios";
 import fileDownload from "js-file-download";
 
 function App() {
   const [downloadProgress, setDownloadProgress] = useState<number>(0);
   const [error, setError] = useState<string | null>(null);
 
-  const handleDownload = async () => {
-    try {
-      const response = await fetch("http://localhost:3001/download");
-      const contentLengthHeader = response.headers.get("Content-Length");
-      const contentLength = contentLengthHeader ? parseInt(contentLengthHeader, 10) : 0;
-
-      if (!contentLength) {
-        throw new Error("Content-Length header is missing or invalid");
-      }
-
-      const reader = response.body!.getReader();
-      let receivedLength = 0;
-
-      // Create a new ReadableStream to handle the data
-      const rs = new ReadableStream({
-        async start(controller) {
-          while (true) {
-            const { done, value } = await reader.read();
-
-            if (done) {
-              // Reset download progress
-              setDownloadProgress(0);
-              controller.close();
-              break;
-            }
-
-            receivedLength += value.length;
-            // Enqueue the next data chunk into our stream
-            controller.enqueue(value);
-            // Calculate download progress
-            const progress = (receivedLength / contentLength) * 100;
-            setDownloadProgress(progress);
-          }
-        },
+  const handleDownload = () => {
+    axios({
+      method: "get",
+      url: "http://localhost:3001/download",
+      responseType: "blob",
+      onDownloadProgress: (progressEvent) => {
+        if (progressEvent.total !== undefined && progressEvent.total !== 0) {
+          setDownloadProgress((progressEvent.loaded * 100) / progressEvent.total);
+        }
+      },
+    })
+      .then((response) => {
+        fileDownload(response.data, "sample.txt");
+        setDownloadProgress(0);
+      })
+      .catch((error) => {
+        setError(`Error downloading file: ${error}`);
       });
-
-      // Create a new Response object from the stream
-      const streamResponse = new Response(rs);
-      // Create a blob from the response
-      const blob = await streamResponse.blob();
-
-      fileDownload(blob, "sample.txt");
-    } catch (error) {
-      setError(`Error downloading file: ${error}`);
-    }
   };
 
   return (
